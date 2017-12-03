7ea9223261684addffc38d2077796d5ea821c28c
Thoreaufare <kolby.rottero@gmail.com>
2017-12-03 02:22:00 -0500

    BSTRAND Updates
    -BSTRAND up to speed with other BSTs. Methods tested and confirmed
    working*
    -currently no memleaks with valgrind
    
    *CATCH is weird and giving me the same random values for the insertion
    decision each time, causing it to occasionally fail a test. Wonder why
    that is. The BSTRAND should work in practice.

---
+++ BSTRAND.h
@@ -0,0 +1,350 @@
+//Implementation of Binary Search Tree that inserts at the root
+//templated with k type of key, v type of value,
+//a comparision function that returns true if first key is less than second key,
+//and an equals function that returns true if first key equals second key
+
+//this BST is NOT self-balancing
+
+#ifndef _BSTRAND_H_
+#define _BSTRAND_H_
+#include <stdexcept>
+
+namespace cop3530 {
+	
+	template <typename k, typename v>
+	class Node {
+		public:
+		k key;
+		v value;
+		Node<k,v> * left = nullptr;
+		Node<k,v> * right = nullptr;
+	};
+	
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	class BST_Rand {
+		public:
+		BST_Rand();
+		~BST_Rand();
+		BST_Rand(const BST_Rand& other); //copy constructor
+		BST_Rand<k,v,comp_func,eq_func>& operator= (const BST_Rand& other); //copy assignment
+		BST_Rand(BST_Rand&& other); //move constructor
+		BST_Rand<k,v,comp_func,eq_func>& operator= (BST_Rand&& other); //move-assignment operator
+		
+		void insert(k key, v value);
+		Node<k,v> * do_insert(Node<k,v> *& root, k key, v value);
+		void remove(k key);
+		Node<k,v> * do_remove(Node<k,v> *& root, k key);
+		v& lookup(k key);
+		v& do_lookup(Node<k,v> *& root, k key);
+		
+		bool contains(k key);
+		bool is_empty();
+		bool is_full();
+		size_t size();
+		size_t do_size(Node<k,v> *& root);
+		void clear();
+		size_t height();
+		size_t do_height(Node<k,v> *& root);
+		//since balance can be negative, and size_t is unsigned, return int instead
+		int balance();
+		int do_balance(Node<k,v> *& root);
+		
+		private:
+		Node<k,v> * head = nullptr;
+	};
+	
+	//--constructors and destructors
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	BST_Rand<k,v,comp_func,eq_func>::BST_Rand() {
+		head = nullptr;
+	}
+	
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	BST_Rand<k,v,comp_func,eq_func>::~BST_Rand() {
+		this->clear();
+	}
+	
+	//--copy constructor
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	BST_Rand<k,v,comp_func,eq_func>::BST_Rand(const BST_Rand& other) {
+		class Node<k,v> *temp;
+		temp = other.head;
+		//TODO insert every key value pair from other to construct new tree
+		
+	}
+	
+	//--copy assignment
+	//may have exception problems if other throws exceptions
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	BST_Rand<k,v,comp_func,eq_func>& BST_Rand<k,v,comp_func,eq_func>::operator=(const BST_Rand& other) {
+		if (this != other) {
+			this->clear();
+			class Node<k,v> *temp;
+			temp = other.head;
+			//TODO insert every key value pair from other to construct new tree
+		}
+		return *this;
+	}
+	
+	//--move constructor
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	BST_Rand<k,v,comp_func,eq_func>::BST_Rand(BST_Rand&& other) {
+		head = other->head;
+		other->head = nullptr;
+	}
+	
+	//--move assignment
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	BST_Rand<k,v,comp_func,eq_func>& BST_Rand<k,v,comp_func,eq_func>::operator=(BST_Rand&& other) {
+		if (this != other) {
+			this->clear();
+			head = other->head;
+			other->head = nullptr;
+		}
+		return *this;
+	}
+	
+	//--insert
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	void BST_Rand<k,v,comp_func,eq_func>::insert(k key, v value) {
+		if (head == nullptr) {
+			head = new Node<k,v>;
+			head->key = key;
+			head->value = value;
+			return;
+		}
+		int size = this->size();
+		int temp = std::rand() % (size+1);
+		if (temp == 0) {
+			//insert at root
+			head = this->do_insert(head, key, value);
+		}
+		else {
+			//insert at leaf
+			if (head == nullptr) {
+			head = new Node<k,v>;
+			head->key = key;
+			head->value = value;
+			return;
+			}
+		
+			//start search for key
+			class Node<k,v> *parent, *temp;
+			parent = head;
+			temp = parent;
+			while (temp != nullptr) {
+				parent = temp;
+				temp = comp_func(key, temp->key) ? temp->left : temp->right;
+			}
+		
+			temp = new Node<k,v>;
+			temp->key = key;
+			temp->value = value;
+			//if temp is less than parent, left child
+			if (comp_func(key, parent->key)) {
+				parent->left = temp;
+			}
+			//else it is parent's right child
+			else {
+				parent->right = temp;
+			}
+		}
+	}
+	
+	//--do_insert
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	Node<k,v> * BST_Rand<k,v,comp_func,eq_func>::do_insert(Node<k,v> *& root, k key, v value) {
+		if (root == nullptr) {
+			root = new Node<k,v>;
+			root->key = key;
+			root->value = value;
+			return root;
+		}
+		
+		if (comp_func(key,root->key)) {
+			root->left = this->do_insert(root->left, key, value);
+			//rotate root right
+			class Node<k,v> *temp;
+			temp = root->left;
+			root->left = temp->right;
+			temp->right = root;
+			root = temp;
+		}
+		else {
+			root->right = this->do_insert(root->right, key, value);
+			//rotate root left
+			class Node<k,v> *temp;
+			temp = root->right;
+			root->right = temp->left;
+			temp->left = root;
+			root = temp;
+		}
+		return root;
+	}
+	
+	//--remove
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	void BST_Rand<k,v,comp_func,eq_func>::remove(k key) {
+		head = this->do_remove(head, key);
+	}
+	
+	//--do_remove
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	Node<k,v>* BST_Rand<k,v,comp_func,eq_func>::do_remove(Node<k,v> *& root, k key) {
+		class Node<k,v> *temp;
+		if (root == nullptr) {
+			return nullptr;
+		}
+		if (comp_func(key,root->key)) {
+			root->left = do_remove(root->left, key);
+		}
+		if (comp_func(root->key, key)) {
+			root->right = do_remove(root->right, key);
+		}
+		if (eq_func(key, root->key)) {
+			temp = root;
+			if (root->right == nullptr) {
+				root = root->left;
+			}
+			else {
+				//find minimum value in right subtree
+				class Node<k,v> *min;
+				min = root->right;
+				while (min->left != nullptr) {
+					min = min->left;
+				}
+				//replace root with min value
+				root->key = min->key;
+				root->value = min->value;
+				//do_remove on right subtree with current root's key, since there is now a duplicate
+				do_remove(root->right, root->key);
+			}
+			delete temp;
+		}
+		return root;	
+	}
+	
+	//--lookup 
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	v& BST_Rand<k,v,comp_func,eq_func>::lookup(k key) {
+		return this->do_lookup(head, key);
+	}
+	
+	//--do_lookup 
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	v& BST_Rand<k,v,comp_func,eq_func>::do_lookup(Node<k,v> *& root, k key) {
+		if (root == nullptr) {
+			throw std::invalid_argument("key does not exist in BST");
+		}
+		if (eq_func(key, root->key)) {
+			return root->value;
+		}
+		if (comp_func(key, root->key)) {
+			return this->do_lookup(root->left, key);
+		}
+		else {
+			return this->do_lookup(root->right, key);
+		}
+	}
+	
+	//--contains
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	bool BST_Rand<k,v,comp_func,eq_func>::contains(k key) {
+		try {
+			this->do_lookup(head, key);
+		}
+		catch (...) {
+			return false;
+		}
+		return true;
+	}
+	
+	//--is_empty
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	bool BST_Rand<k,v,comp_func,eq_func>::is_empty() {
+		if (head == nullptr) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	//--is_full
+	//should only be full if we can not physically allocate more memory for a new node
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	bool BST_Rand<k,v,comp_func,eq_func>::is_full() {
+		class Node<k,v> *temp;
+		try {
+			
+			temp = new Node<k,v>; //would throw an exception with 'new' if out of memory
+		}
+		catch (...) {
+			return true;
+		}
+		delete temp;
+		return false;
+	}
+	
+	//--size
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	size_t BST_Rand<k,v,comp_func,eq_func>::size() {
+		return this->do_size(head);
+	}
+	
+	//--do_size
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	size_t BST_Rand<k,v,comp_func,eq_func>::do_size(Node<k,v> *& root) {
+		if (root == nullptr) {
+			return 0;
+		}
+		size_t temp = 1;
+		temp += this->do_size(root->left);
+		temp += this->do_size(root->right);
+		return temp;
+	}
+	
+	//--clear
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	void BST_Rand<k,v,comp_func,eq_func>::clear() {
+		//should work by just calling remove on head
+		while (head != nullptr) {
+			this->remove(head->key);
+		}
+	}
+	
+	//--height
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	size_t BST_Rand<k,v,comp_func,eq_func>::height() {
+		return this->do_height(head);
+	}
+	
+	//--do_height
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	size_t BST_Rand<k,v,comp_func,eq_func>::do_height(Node<k,v> *& root) {
+		if (root == nullptr) {
+			return 0;
+		}
+		size_t temp = 1;
+		temp += std::max(this->do_height(root->left), this->do_height(root->right));
+		return temp;
+	}
+	
+	//--balance
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	int BST_Rand<k,v,comp_func,eq_func>::balance() {
+		return this->do_balance(head);
+	}
+	
+	//--do_balance
+	template <typename k, typename v, bool (*comp_func)(k,k), bool (*eq_func)(k,k)>
+	int BST_Rand<k,v,comp_func,eq_func>::do_balance(Node<k,v> *& root) {
+		if (root == nullptr) {
+			return 0;
+		}
+		int left_height = this->do_height(root->left);
+		int right_height = this->do_height(root->right);
+		return left_height - right_height;
+	}
+}
+#endif
---
+++ BSTRAND_main.cpp
@@ -0,0 +1,280 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "BSTRAND.h"
+
+#include <iostream>
+
+using namespace cop3530;
+
+bool comparison_function(const int a, const int b) {
+	if (a < b) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool equals_function(const int a, const int b) {
+	if (a == b) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+SCENARIO("Testing Insertion and Lookup") {
+	GIVEN ("List of items, with keys being [1,19,5,18,3,8,9] and values being [A,S,E,R,C,H,I]") {
+		BST_Rand<int, char, comparison_function, equals_function> * bst = new BST_Rand<int, char, comparison_function, equals_function>;
+		
+		bst->insert(1,'A');
+		bst->insert(19,'S');
+		bst->insert(5,'E');
+		bst->insert(18,'R');
+		bst->insert(3,'C');
+		bst->insert(8,'H');
+		bst->insert(9,'I');
+		
+		WHEN("Looking up 5") {
+			char value = bst->lookup(5);
+			THEN ("The value should be 'E' ") {
+				REQUIRE(value == 'E');
+			}
+		}
+		
+		WHEN("Looking up 9") {
+			char value = bst->lookup(9);
+			THEN ("The value should be 'I' ") {
+				REQUIRE(value == 'I');
+			}
+		}
+		
+		WHEN("Looking up 1") {
+			char value = bst->lookup(1);
+			THEN ("The value should be 'A' ") {
+				REQUIRE(value == 'A');
+			}
+		}
+		
+		WHEN("Looking up 19") {
+			char value = bst->lookup(19);
+			THEN ("The value should be 'S' ") {
+				REQUIRE(value == 'S');
+			}
+		}
+		
+		WHEN("Looking up 18") {
+			char value = bst->lookup(18);
+			THEN ("The value should be 'R' ") {
+				REQUIRE(value == 'R');
+			}
+		}
+		
+		WHEN("Looking up 3") {
+			char value = bst->lookup(3);
+			THEN ("The value should be 'C' ") {
+				REQUIRE(value == 'C');
+			}
+		}
+		
+		WHEN("Looking up 8") {
+			char value = bst->lookup(8);
+			THEN ("The value should be 'H' ") {
+				REQUIRE(value == 'H');
+			}
+		}
+		
+		delete bst;
+	}
+}
+
+SCENARIO("Testing removal") {
+	GIVEN ("List of items, with keys being [1,19,5,18,3,8,9] and values being [A,S,E,R,C,H,I]") {
+		BST_Rand<int, char, comparison_function, equals_function> * bst = new BST_Rand<int, char, comparison_function, equals_function>;
+		
+		bst->insert(1,'A');
+		bst->insert(19,'S');
+		bst->insert(5,'E');
+		bst->insert(18,'R');
+		bst->insert(3,'C');
+		bst->insert(8,'H');
+		bst->insert(9,'I');
+		
+		/*
+		WHEN("Removing 1") {
+			bst->remove(1);
+			THEN("Looking up 1 should throw an exception") {
+				bst->lookup(1);
+			}
+		}
+		*/
+		
+		WHEN("Removing 1") {
+			bst->remove(1);
+			THEN("Looking up 18 should NOT throw an exception") {
+				char value = bst->lookup(18);
+				REQUIRE(value == 'R');
+			}
+		}
+		
+		WHEN("Removing 18") {
+			bst->remove(1);
+			THEN("Looking up 9 should NOT throw an exception") {
+				char value = bst->lookup(9);
+				REQUIRE(value == 'I');
+			}
+		}
+		
+		/*
+		WHEN("Removing 9") {
+			bst->remove(9);
+			THEN("Looking up 9 should throw an exception") {
+				char value = bst->lookup(9);
+			}
+		}
+		*/
+		
+		WHEN("Removing 9") {
+			bst->remove(9);
+			THEN("Looking up 3 should NOT throw an exception") {
+				char value = bst->lookup(3);
+				REQUIRE(value == 'C');
+			}
+		}
+		
+		WHEN("Removing 18") {
+			bst->remove(18);
+			THEN("Looking up 8 should NOT throw an exception") {
+				bst->lookup(8);
+			}
+		}
+		
+		WHEN("Removing 19") {
+			bst->remove(19);
+			THEN("Looking up 18 should NOT throw an exception") {
+				bst->lookup(18);
+			}
+		}
+		
+		delete bst;
+	}
+}
+
+SCENARIO("Testing bonus methods") {
+	GIVEN ("List of items, with keys being [1,19,5,18,3,8,9] and values being [A,S,E,R,C,H,I]") {
+		BST_Rand<int, char, comparison_function, equals_function> * bst = new BST_Rand<int, char, comparison_function, equals_function>;
+		
+		bst->insert(1,'A');
+		bst->insert(19,'S');
+		bst->insert(5,'E');
+		bst->insert(18,'R');
+		bst->insert(3,'C');
+		bst->insert(8,'H');
+		bst->insert(9,'I');
+		
+		WHEN("Checking contains for 8") {
+			bool x = bst->contains(8);
+			THEN("Contains should return true") {
+				REQUIRE(x == true);
+			}
+		}
+		
+		WHEN("Checking contains for 22") {
+			bool x = bst->contains(22);
+			THEN("Contains should return false") {
+				REQUIRE(x == false);
+			}
+		}
+		
+		WHEN("Checking is_empty") {
+			bool x = bst->is_empty();
+			THEN("Is_empty should return false") {
+				REQUIRE(x == false);
+			}
+		}
+		
+		WHEN("Clearing and checking is_empty") {
+			bst->clear();
+			bool x = bst->is_empty();
+			THEN("Is_empty should return true") {
+				REQUIRE(x == true);
+			}
+		}
+		
+		WHEN("Checking is_full") {
+			bool x = bst->is_full();
+			THEN("Is_full should return false") {
+				REQUIRE(x == false);
+			}
+		}
+		
+		WHEN("Checking size") {
+			size_t size = bst->size();
+			THEN("The size should be 7 (there are 7 items)") {
+				REQUIRE(size == 7);
+			}
+		}
+		
+		WHEN("Checking height") {
+			size_t height = bst->height();
+			THEN("Height should be likely not be 6 or 4") {
+				REQUIRE(height != 6);
+				REQUIRE(height != 4);
+			}
+		}
+		
+		WHEN("Checking balance") {
+			int balance = bst->balance();
+			THEN("Balance should likely not be -5 or 1") {
+				REQUIRE(balance != -5);
+				REQUIRE(balance != 1);
+			}
+		}
+		
+		delete bst;
+	}
+}
+/*
+SCENARIO("Testing 'big five' methods") {
+	GIVEN("List of items, with keys being [1,19,5,18,3,8,9] and values being [A,S,E,R,C,H,I]") {
+		BST_Rand<int, char, comparison_function, equals_function> * bst = new BST_Rand<int, char, comparison_function, equals_function>;
+		
+		bst->insert(1,'A');
+		bst->insert(19,'S');
+		bst->insert(5,'E');
+		bst->insert(18,'R');
+		bst->insert(3,'C');
+		bst->insert(8,'H');
+		bst->insert(9,'I');
+		
+		WHEN("Testing copy constructor") {
+			BST_Rand<int, char, comparison_function, equals_function> * bst2 = bst;
+			THEN("New copy should successfully lookup 18") {
+				char value = bst2->lookup(18);
+				REQUIRE(value == 'R');
+			}
+		}
+		
+		WHEN("Testing copy assignment") {
+			BST_Rand<int, char, comparison_function, equals_function> * bst2 = new BST_Rand<int, char, comparison_function, equals_function>;
+			//throw in values different from other to ensure assignment works
+			bst2->insert(4,'D');
+			bst2->insert(2,'B');
+			bst2->insert(26,'Z');
+			
+			bst2 = bst;
+			THEN("New copy should succesfully lookup 18") {
+				char value = bst2->lookup(18);
+				REQUIRE(value == 'R');
+			}
+			
+			delete bst2;
+		}
+		
+		
+		delete bst;
+	}
+}
+*/
